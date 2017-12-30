33bfe76ab4ef0b4c42b42b86eaa0a51a3fee94c7
duangsuse <fedora-opensuse@outlook.com>
2017-12-30 13:05:09 +0800

    Design: Update structure

--- src/min/vm/exceptions/OpException.java
+++ src/min/vm/exceptions/OpException.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package min.vm.internal;
+package min.vm.exceptions;
 
 /**
  *
  * @author dse
  */
-public class MinProc {
+public class OpException {
     
 }
